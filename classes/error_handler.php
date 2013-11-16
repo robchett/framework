5ec0412e7fb3d177331261875a6ea9f23fde5a1e
Rob <robchett@gmail.com>
2013-11-16 17:35:00 +0000

    Add an error_log to the error_handler class.

--- classes/error_handler.php
+++ classes/error_handler.php
@@ -3,13 +3,19 @@ namespace core\classes;
 
 class error_handler {
 
+    public static $file_handler;
+
     public static function handle_error($errno, $errstr, $errfile, $errline) {
+        if(!self::$file_handler) {
+            self::$file_handler = fopen(root . '/log/error.log', 'w');
+        }
         if (strpos($errfile, 'xdebug') !== 0) {
             if (function_exists('xdebug_break')) {
                 xdebug_break();
             }
             require_once(root . '/.core/core.php');
             $error = '<div class="error_message mysql"><p>Error #' . $errno . ' "' . $errstr . '" in ' . $errfile . ' on line ' . $errline . '</p>' . \core::get_backtrace() . '</div>';
+            fwrite(self::$file_handler, $error . "\n\n\n---------------\n\n\n");
             if (ajax) {
                 require_once(root . '/.core/classes/ajax.php');
                 \classes\ajax::inject('body', 'append', $error);
