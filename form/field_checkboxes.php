0c00e58ab6f2034b3bc4c32df6a78d384bf22f8b
Rob <robchett@gmail.com>
2013-12-04 19:25:30 +0000

    Allow for groupings in field_checkboxes.

--- form/field_checkboxes.php
+++ form/field_checkboxes.php
@@ -13,11 +13,27 @@ abstract class field_checkboxes extends field {
 
     public function get_html() {
         $html = '';
-        foreach ($this->options as $k => $v) {
-            $html .= '<label class="checkbox"><input type="checkbox" name="' . $this->field_name . '[]" value="' . $k . '" ' . (in_array($k, $this->parent_form->{$this->field_name}) ? 'checked="checked"' : '') . '>' . $v . '</label>' . "\n";
-
+        foreach ($this->options as $key => $value) {
+            if(is_array($value)) {
+                $html .= '<div class="checkboxes_wrapper">';
+                $html .= '<span class="legend">' . $key . '</span>';
+                foreach($value as $_key => $_value) {
+                    $html .= $this->get_inner_html($_key, $_value);
+                }
+                $html .= '</div>';
+            } else {
+                $html .= $this->get_inner_html($key, $value);
+            }
         }
         return $html;
+    }
+
+    protected function get_inner_html($key, $value) {
+        return '
+        <label class="checkbox">
+            <input type="checkbox" name="' . $this->field_name . '[]" value="' . $key . '" ' . (in_array($key, $this->parent_form->{$this->field_name}) ? 'checked="checked"' : '') . '>'
+            . $value . '
+        </label>';
     }
 
     public function set_from_request() {
