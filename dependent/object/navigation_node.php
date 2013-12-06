02d85c8a27d537d470f1acaa5bc02b43b077ac32
Rob <robchett@gmail.com>
2013-12-06 19:23:43 +0000

    Add core navigation_link

---
+++ dependent/object/navigation_node.php
@@ -0,0 +1,7 @@
+<?php
+namespace object;
+
+class navigation_node extends \core\object\navigation_node {
+
+}
+ 
---
+++ object/navigation_node.php
@@ -0,0 +1,42 @@
+<?php
+namespace core\object;
+
+use classes\table;
+use html\node;
+use object\navigation_node as _navigation_node;
+use traits\table_trait;
+
+class navigation_node extends table {
+
+    use table_trait;
+
+    public $link;
+    public $title;
+
+    public function get_title() {
+        return $this->title;
+    }
+
+    public function get_url() {
+        return $this->link;
+    }
+
+    public static function get_list($nnid = 0) {
+        $nodes = _navigation_node::get_all([], ['where_equals' => ['parent_nnid' => $nnid]]);
+        if ($nodes->count()) {
+            return node::create('ul', [],
+                $nodes->iterate_return(
+                    function (_navigation_node $node) {
+                        return node::create('li', [],
+                            node::create('a', ['href' => $node->get_url()], $node->get_title()) .
+                            static::get_list($node->get_primary_key())
+                        );
+                    }
+                )
+            );
+        } else {
+            return '';
+        }
+    }
+}
+ 
