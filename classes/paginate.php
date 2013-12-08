c77b179779b0b33230a1c722fd3f971be8e5db77
Rob <robchett@gmail.com>
2013-12-08 11:57:00 +0000

    Added paginate titles and a standard wrapper

--- classes/paginate.php
+++ classes/paginate.php
@@ -15,26 +15,28 @@ class paginate {
     public $post_title;
 
     public function get() {
-        $node = node::create('div');
+        $node = node::create('div.paginate_wrapper');
         if ($this->npp && $this->total > $this->npp) {
             $pages = ceil($this->total / $this->npp);
             if ($this->title) {
-                $node->nest(node::create('span.title', [], $this->do_replace($this->title)));
+                $node->add_child(node::create('span.title', [], $this->do_replace($this->title)));
             }
             if ($pages > 40) {
-                $node = node::create('select#pagi.cf', ['data-ajax-change' => $this->act]);
+                $_node = node::create('select', ['data-ajax-change' => $this->act]);
                 for ($i = 1; $i <= $pages; $i++) {
                     $attributes = ['value' => $i];
                     if ($this->page = $i) {
                         $attributes['selected'] = 'selected';
                     }
-                    $node->add_child(node::create('option', ['value' => $i], $i));
+                    $_node->add_child(node::create('option', ['value' => $i], $i));
                 }
+                $node->add_child($_node);
             } else {
-                $node = node::create('ul#pagi.cf');
+                $_node = node::create('ul#pagi.cf');
                 for ($i = 1; $i <= $pages; $i++) {
-                    $node->add_child(node::create('li' . ($this->page == $i ? '.sel' : '') . ' a', ['href' => '/' . trim($this->base_url, '/') . '/page/' . $i], $i));
+                    $_node->add_child(node::create('li' . ($this->page == $i ? '.sel' : '') . ' a', ['href' => '/' . trim($this->base_url, '/') . '/page/' . $i], $i));
                 }
+                $node->add_child($_node);
             }
             if ($this->post_title) {
                 $node->nest(node::create('span.title', [], $this->do_replace($this->post_title)));
