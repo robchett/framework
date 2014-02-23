630138279028bc54085d0c1f6b96551557045f20
Robert Chettleburgh <robchett@gmail.com>
2014-02-23 21:41:30 +0000

    Add caching to autoloader paths

--- classes/auto_loader.php
+++ classes/auto_loader.php
@@ -4,26 +4,34 @@ namespace core\classes;
 
 abstract class auto_loader {
 
+    protected static $file_paths = [];
+
     public function __construct() {
         spl_autoload_register(['self', 'load']);
+        $this->load_cache();
     }
 
     public function load($class) {
-        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+        if (isset(static::$file_paths[$class])) {
+            $path = static::$file_paths[$class];
+        } else {
 
-        $path = false;
-        $local_path = root . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . $class . '.php';
-        $dependent_path = root . DIRECTORY_SEPARATOR . '.core' . DIRECTORY_SEPARATOR . 'dependent' . DIRECTORY_SEPARATOR . $class . '.php';
-        $core_path = root . DIRECTORY_SEPARATOR . str_replace('core', '.core', $class) . '.php';
-        $library_path = root . DIRECTORY_SEPARATOR . 'library' . DIRECTORY_SEPARATOR . $class . '.php';
-        if (is_readable($local_path)) {
-            $path = $local_path;
-        } else if (is_readable($dependent_path)) {
-            $path = $dependent_path;
-        } else if (is_readable($core_path)) {
-            $path = $core_path;
-        } else if (is_readable($library_path)) {
-            $path = $library_path;
+            $class_path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+            $path = false;
+            $local_path = root . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . $class_path . '.php';
+            $dependent_path = root . DIRECTORY_SEPARATOR . '.core' . DIRECTORY_SEPARATOR . 'dependent' . DIRECTORY_SEPARATOR . $class_path . '.php';
+            $core_path = root . DIRECTORY_SEPARATOR . str_replace('core', '.core', $class_path) . '.php';
+            $library_path = root . DIRECTORY_SEPARATOR . 'library' . DIRECTORY_SEPARATOR . $class_path . '.php';
+            if (is_readable($local_path)) {
+                $path = $local_path;
+            } else if (is_readable($dependent_path)) {
+                $path = $dependent_path;
+            } else if (is_readable($core_path)) {
+                $path = $core_path;
+            } else if (is_readable($library_path)) {
+                $path = $library_path;
+            }
+            static::$file_paths[$class] = $path;
         }
 
         if ($path) {
@@ -32,5 +40,18 @@ abstract class auto_loader {
         } else {
             return false;
         }
+    }
+
+    public function load_cache() {
+        try {
+            static::$file_paths = \classes\cache::get('autoloader.file_paths', ['autoloader']);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function __destruct() {
+        \classes\cache::set(['autoloader.file_paths' => static::$file_paths], ['autoloader']);
     }
 }
