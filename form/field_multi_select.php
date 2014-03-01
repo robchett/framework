915febf1d28df402f6e128a5743496216931e7ca
Rob <robchett@gmail.com>
2014-03-01 21:35:24 +0000

    Correct set from request when used out  of raw mode - ie not CMS

--- form/field_multi_select.php
+++ form/field_multi_select.php
@@ -9,12 +9,18 @@ abstract class field_multi_select extends field {
     public function get_html() {
         $html = '';
         $html .= '<select id="' . $this->field_name . '" name="' . $this->field_name . '" class="picker' . ($this->required ? ' false' : '') . '" multiple="multiple">' . "\n";
-        if (!empty($this->default)) $html .= '<option value="default">' . $this->default . '</option>' . "\n";
+        if (!empty($this->default)) {
+            $html .= '<option value="default">' . $this->default . '</option>' . "\n";
+        }
         foreach ($this->options as $k => $v) {
             $html .= '<option value="' . $k . '" ' . (in_array($k, $this->value) ? 'selected="selected"' : '') . '>' . $v . '</option>' . "\n";
 
         }
         $html .= '</select>' . "\n";
         return $html;
+    }
+
+    public function set_from_request() {
+        $this->parent_form->{$this->field_name} = (isset($_REQUEST[$this->field_name]) ? $_REQUEST[$this->field_name] : []);
     }
 }
