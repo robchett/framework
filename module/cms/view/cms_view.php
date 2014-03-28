d29e0642bad7b7c631ad0a95d28649c19eaab0fc
Robert Chettleburgh <robchett@gmail.com>
2014-03-28 20:54:57 +0000

    Be more clever when generating CMS titletags

--- module/cms/view/cms_view.php
+++ module/cms/view/cms_view.php
@@ -1,8 +1,11 @@
 <?php
 namespace core\module\cms\view;
 
+use classes\get;
+
 /**
  * Class cms_view
+ *
  * @package cms
  * @property \module\cms\controller $module
  */
@@ -16,6 +19,17 @@ abstract class cms_view extends \template\cms\html {
     }
 
     public function get_title_tag() {
-        return parent::get_title_tag() . ' | CMS | ' . ucwords(str_replace('_', ' ', \classes\get::__class_name($this))) . (isset($this->module->current) ? ' | ' . $this->module->current->get_title() : '');
+        $parts = [parent::get_title_tag(), 'CMS', ucwords(str_replace('_', ' ', \classes\get::__class_name($this)))];
+        if (isset($this->module->current)) {
+            $parts[] = ucwords(str_replace('_', ' ', get::__class_name($this->module->current)));
+            if ($this->module->current->get_primary_key()) {
+                if ($title = $this->module->current->get_title()) {
+                    $parts[] = $title;
+                } else {
+                    $parts[] = $this->module->current->get_primary_key();
+                }
+            }
+        }
+        return implode(' | ', $parts);
     }
 }
