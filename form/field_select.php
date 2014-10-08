9aec3fd5f69fe5819e1af2cebe0796eb7e0f64f9
Robert Chettleburgh <robchett@gmail.com>
2014-10-08 18:59:34 +0100

    Formating field_select

--- form/field_select.php
+++ form/field_select.php
@@ -5,6 +5,7 @@ abstract class field_select extends \form\field {
 
     public $default = 'Please Choose';
     public $options = [];
+    public $attributes = [];
 
     public function  __construct($title, $options = []) {
         parent::__construct($title, $options);
@@ -15,7 +16,9 @@ abstract class field_select extends \form\field {
         $attributes = $this->attributes;
         $this->set_standard_attributes($attributes);
         $html = '<select ' . static::get_attributes($attributes) . '>' . "\n";
-        if (!empty($this->default) && !$this->required) $html .= '<option value="default">' . $this->default . '</option>' . "\n";
+        if (!empty($this->default) && !$this->required) {
+            $html .= '<option value="default">' . $this->default . '</option>' . "\n";
+        }
         foreach ($this->options as $k => $v) {
             $html .= '<option value="' . $k . '" ' . ($this->parent_form->{$this->field_name} == $k ? 'selected="selected"' : '') . '>' . $v . '</option>' . "\n";
 
@@ -27,7 +30,8 @@ abstract class field_select extends \form\field {
     public function do_validate(&$error_array) {
         /*if ($this->required && (empty($this->parent_form->{$this->field_name})))
             $error_array[$this->field_name] = $this->field_name . ' is required field';*/
-        if ($this->parent_form->{$this->field_name} == 'default' && $this->required)
+        if ($this->parent_form->{$this->field_name} == 'default' && $this->required) {
             $error_array[$this->field_name] = $this->field_name . ' please choose an option';
+        }
     }
 }
